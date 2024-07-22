6b3d32ee249b9ecf4f51f76eb84cd6ac27d7d134
gvbvdxx <gvbvdxx@outlook.com>
2024-07-22 19:34:10 -0400

    Upload main source code.

---
+++ generated-upload-script.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Pushing scratch-render to github...
+git lfs uninstall
+git init
+git rm -r --cached .
+git add .
+git commit -m "Upload main source code."
+git branch -M main
+git remote add origin https://github.com/gvbmod/scratch-render.git
+git push -f --no-verify origin main
